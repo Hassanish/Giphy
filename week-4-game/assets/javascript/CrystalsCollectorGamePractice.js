acf94b6122c48526c1f81478f5329f74d0c30e60
Hassanish <36898041+Hassanish@users.noreply.github.com>
2018-03-12 17:12:55 -0500

    Add files via upload

---
+++ week-4-game/assets/javascript/CrystalsCollectorGamePractice.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+
+    var userRandomCounts =0;
+    var wins = 0;
+    var losses = 0;
+    $('#winning').text(wins);
+    $('#losser').text(losses);
+   
+    Start();
+    Reset();
+    function Start () {
+        var numbers = []
+            while(numbers.length < 4){
+              var randomnumber = Math.floor((Math.random()* 11) + 1);
+             var randomguesses = false;
+                    for (var i=0; i< numbers.length; i++){ 
+                        if (numbers[i] == randomnumber){
+                            rundomguesses = true;}}
+                            if(!randomguesses)numbers[numbers.length]=randomnumber;
+                     }
+            crystals = ['file:///C:/Users/sinyeiman/Pictures/RedCrystal.png.docx.png',
+            'BlueCrystal.png.docx.png',
+            'file:///C:/Users/sinyeiman/Pictures/YellowCrystal.png.docx.png',
+            'file:///C:/Users/sinyeiman/Pictures/GreenCrystal.png.docx.png'];            
+        console.log(numbers);       
+        for (i = 0; i < numbers.length; i++) {
+            var Crystall = $('<img>');
+            Crystall.attr('data-num', numbers[i]);
+            Crystall.attr('src', crystals[i]);
+            Crystall.attr('alt', 'crystals');
+            Crystall.addClass('EachCrystal')
+            $('#crystals').append(Crystall);}
+            }
+    function Reset() {
+
+        userRandomCounts= 0;
+        $('#TScore').text(userRandomCounts);
+
+        var numberToGuess = Math.floor((Math.random()* 100) + 19);
+        
+        $('.ComputerRandomNumber').text(numberToGuess);
+
+        $('.EachCrystal').on('click', function(){
+            userRandomCounts = userRandomCounts + parseInt($(this).data('num'));
+           
+            $('#TScore').text(userRandomCounts);
+
+            if (userRandomCounts == numberToGuess){
+              $('#Results').text('Yee you won!');
+              wins ++;
+              $('#winning').text(wins);
+              alert("You won!"); 
+              console.log(wins)
+              $('#crystals').empty();
+              Start();
+              Reset();
+                
+            } else if (userRandomCounts> numberToGuess){
+                $('#Results').text('You lose!')
+                losses ++;
+                alert ("Oops you lost!");
+  
+                $('#losser').text(losses);
+                console.log(losses)
+                $('#crystals').empty();
+                Start();
+                Reset();
+               
+            }
+        });
+    }
+
+});
