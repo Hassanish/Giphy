22105d0cf8efec8df2123120607cd6f86b8e29e5
Hassanish <36898041+Hassanish@users.noreply.github.com>
2018-03-12 21:52:38 -0500

    Update CrystalsCollectorGamePractice.js

--- week-4-game/assets/javascript/CrystalsCollectorGamePractice.js
+++ week-4-game/assets/javascript/CrystalsCollectorGamePractice.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
                             rundomguesses = true;}}
                                if(!randomguesses)numbers[numbers.length]=randomnumber;
                      }
-    crystals = ['RedCrystal.png.docx.png',
+    crystals = ['https://github.com/Hassanish/HW-Wireframe/blob/master/week-4-game/assets/images/RedCrystal.png.docx.png',
             'BlueCrystal.png.docx.png',
             'YellowCrystal.png.docx.png',
             'GreenCrystal.png.docx.png'];            
